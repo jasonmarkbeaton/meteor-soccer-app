d56262a870a6f7b360a3f0953771b7e1b5476c04
Jason Beaton <jasonbeaton@classrotechsmbp.wireless.dartmouth.edu>
2017-01-18 14:44:00 -0500

    added react router and basic setup

---
+++ imports/ui/Example.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Example extends Component {
+  render() {
+    return (
+      <h1>Here's an example</h1>
+    )
+  }
+}
---
+++ imports/ui/Lost.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Lost extends Component {
+  render() {
+    return (
+      <h1>You're not found</h1>
+    )
+  }
+}
